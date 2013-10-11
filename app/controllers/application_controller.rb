4d7c5cf28c7afe840f00fcabee5faa4b13709025
Sadique Ali <sadiqalikm@gmail.com>
2013-10-11 00:11:01 -0700

    Simple throttling using before filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,20 @@ class ApplicationController < ActionController::Base
 
   def throttle
     client_ip = request.env["REMOTE_ADDR"]
-    REDIS.incr "count:#{client_ip}"
+    key = "count:#{client_ip}"
+    count = REDIS.get(key)
+
+    unless count
+      REDIS.set(key, 1)
+      REDIS.expire(key, THROTTLE_TIME_WINDOW)
+      return true
+    end
+
+    if count.to_i >= THROTTLE_MAX_REQUESTS
+      render :json => {:message => "You have fired too many requests. Please wait for some time."}, :status => 429
+      return
+    end
+    REDIS.incr(key)
     true
   end
 end
