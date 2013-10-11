d54b425761a0d804791dd533bb36bb9c892a2a22
Sadique Ali <sadiqalikm@gmail.com>
2013-10-10 23:24:05 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsThrottle::Application.config.secret_key_base = '7f6cc8be659433f89b5312d9f2ec1d501c60c70988208053def0c254af1ff59407843765a9a2d1b911fec71ddb85158ba11d686b8d97d423dfb0e6b096f80e5d'
