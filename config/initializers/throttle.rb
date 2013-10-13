c480633b091025863aec54be094507b2528fcd4b
Sadique Ali <sadiqalikm@gmail.com>
2013-10-13 00:33:27 -0700

    Fixed the time limit to 15 * 60 seconds.

--- config/initializers/throttle.rb
+++ config/initializers/throttle.rb
@@ -3,5 +3,5 @@ require "redis"
 redis_conf  = YAML.load(File.join(Rails.root, "config", "redis.yml"))
 REDIS = Redis.new(:host => redis_conf["host"], :port => redis_conf["port"])
 
-THROTTLE_TIME_WINDOW = 15 * 60 * 60
+THROTTLE_TIME_WINDOW = 15 * 60
 THROTTLE_MAX_REQUESTS = 60
